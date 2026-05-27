587b286553f2f67a2194f3fe0c69e37f7c11d880
Joseph Sintum <sjumbam15@gmail.com>
2026-05-26 17:16:30 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    
+	for i := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			sum := nums[i] + nums[j]
+			if sum == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
